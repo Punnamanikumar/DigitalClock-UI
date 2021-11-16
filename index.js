63fd07f65abc0e77b35ccbbb88177ef8d4e0c22d
manikumar04-10-2021 <17n81a05j7manikumar@gmail.com>
2021-11-16 20:09:11 +0530

    AM PM Changes

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ function timer() {
     let m = today.getMinutes();
     let s = today.getSeconds();
     // let d=today.get
-
+    h = checkTime1(h)
     m = checkTime(m);
     s = checkTime(s);
 
 
     const h1 = document.getElementById("hours")
-    h1.innerText = (h - 12) + "\n" + "hrs";
-    console.log(h1.innerText);
+    h1.innerText = h + "\n" + "hrs";
+    // console.log(h1.innerText);
 
 
 
     const m1 = document.getElementById("minutes")
     m1.innerText = m + "\n" + "min";
-    console.log(m1.innerText);
+    // console.log(m1.innerText);
 
     const s1 = document.getElementById("seconds")
     s1.innerText = s + "\n" + "sec";
-    console.log(s1.innerText);
+    // console.log(s1.innerText);
     setTimeout(timer, 1000);
 
     function checkTime(i) {
@@ -30,4 +30,31 @@ function timer() {
         }
         return i;
     }
+
+    function checkTime1(i) {
+        if (i > 12) {
+            i = i - 12;
+        }
+        return i;
+    }
+
+    // function day() {
+    //     let d = today.getHours;
+    //     if (d < 12) {
+    //         console.log("PM");
+    //     } else
+    //         console.log("AM");
+
+    // }
+    // day();
+    //FOR AM and PM
+    let d = today.getHours;
+    var ampm = d >= 12 ? 'AM' : 'PM';
+    const e = document.getElementById("day");
+    e.innerText = ampm;
+}
+
+function wakeUpTime() {
+    var wkup = document.getSelection(".wakeup").value;
+    console.log(wkup);
 }
